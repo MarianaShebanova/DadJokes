ab4fbbefa3c9f95d67759085bf51c328994cfa44
Mariana Shebanova <mariana.shebanova@gmail.com>
2021-07-07 11:20:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+const generateJoke = function () {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    })
+    .catch((error) => {
+      jokeEl.innerHTML = "Error";
+    });
+};
+
+generateJoke();
+
+jokeBtn.addEventListener("click", generateJoke);
